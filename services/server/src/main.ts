f85f7ef71566299218dc22e3144beba67651096e
Assaf Koren <assafnoahkoren@gmail.com>
2024-02-20 01:28:15 +0200

    added swagger

--- services/server/src/main.ts
+++ services/server/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	const port = parseInt(process.env.PORT || process.env.SERVER_PORT) || 3000;
+
+	const config = new DocumentBuilder()
+		.setTitle('Apprentice')
+		.setDescription('API description')
+		.setVersion('1.0')
+		.build();
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('docs', app, document);
+	
 	await app.listen(port);
 	console.log(`Application is running on port: ${port}`);	
 	
