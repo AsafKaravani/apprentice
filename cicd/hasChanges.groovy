62e3db505fabcddf82380a0c836a430e6fbc56cb
Assaf Koren <assafnoahkoren@gmail.com>
2024-02-27 02:06:48 +0200

    .

--- cicd/hasChanges.groovy
+++ cicd/hasChanges.groovy
@@ -1,4 +1,6 @@
 // Function to check if there are changes in a specific directory
-def call(String directory) {
+def checkChanges(String directory) {
     return sh(script: "git diff --quiet HEAD HEAD~ -- ${directory}/", returnStatus: true) != 0
 }
+
+return this
