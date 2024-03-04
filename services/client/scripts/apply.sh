709e6a87bda010ad5a334e966a223dd361beabae
Assaf Koren <assafnoahkoren@gmail.com>
2024-03-05 01:44:56 +0200

    .

--- services/client/scripts/apply.sh
+++ services/client/scripts/apply.sh
@@ -1,5 +1,6 @@
 cd ../hasura
-# print current directory
-pwd
-ls
-yarn hasura metadata apply --admin-secret "$HASURA_GRAPHQL_ADMIN_SECRET" --endpoint "$HASURA_ENDPOINT" --log-level DEBUG --project .
+# create var with the absolute current path
+current_dir=$(dirname "$(realpath "$0")")
+
+
+yarn hasura metadata apply --admin-secret "$HASURA_GRAPHQL_ADMIN_SECRET" --endpoint "$HASURA_ENDPOINT" --log-level DEBUG --project "$current_dir"
