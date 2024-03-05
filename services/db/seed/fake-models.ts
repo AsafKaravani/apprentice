45913bc9c284e2286de4c6f6af3dfd3b0e463998
Assaf Noah Koren <assafnoahkoren@gmail.com>
2024-03-05 10:21:07 +0200

    .

--- services/db/seed/fake-models.ts
+++ services/db/seed/fake-models.ts
@@ -1,5 +1,6 @@
 import * as fake from '@ngneat/falso';
-import { Profile } from "@prisma/client";
+import { Profile, Organization, Group,GroupMember, OrganizationMember, Exam, Question  } from "@prisma/client";
+import { group } from 'console';
 
 export const fakeModels = {
 	profile: (): Profile => {
@@ -14,5 +15,90 @@ export const fakeModels = {
 			updated_at: new Date(),
 			created_at: new Date(),
 		}
+	},
+
+	organization: (): Organization => {
+		const email = fake.randEmail();
+		return {
+			id: fake.randUuid(),
+			name: fake.randCompanyName(),
+			email: email,
+			phone: fake.randPhoneNumber(),
+			description: fake.randParagraph(),
+			picture_url: `https://picsum.photos/seed/${email}/200/300`,
+			website: fake.randUrl(),
+			updated_at: new Date(),
+			created_at: new Date(),
+		}
+	},
+
+	group: (): Group => {
+		return {
+			id: fake.randUuid(),
+			name: fake.randCompanyName(),
+			email: fake.randEmail(),
+			phone: fake.randPhoneNumber(),
+			picture_url: fake.randUrl(),
+			website: fake.randUrl(),
+			description: fake.randParagraph(),
+			updated_at: new Date(),
+			created_at: new Date(),
+		}
+	},
+
+	question_MultipleChoice: (examId: string): Question => {
+		return {
+			id: fake.randUuid(),
+			question: fake.randParagraph(),
+			type: 'MULTIPLE_CHOICE',
+			options: [],
+			correct_options: [],
+
+			boolean_expected_answer: null,
+			expected_answer: null,
+			image_url: null,
+			exam_id: examId,
+
+			updated_at: new Date(),
+			created_at: new Date(),
+		}
+	},
+
+	question_TrueFalse: (examId: string): Question => {
+		return {
+			id: fake.randUuid(),
+			question: fake.randParagraph(),
+			type: 'TRUE_FALSE',
+			boolean_expected_answer: null,
+			
+			options: [],
+			correct_options: [],
+			expected_answer: null,
+			image_url: null,
+
+			exam_id: examId,
+
+			updated_at: new Date(),
+			created_at: new Date(),
+		}
+	},
+
+	question_FreeText: (examId: string): Question => {
+		return {
+			id: fake.randUuid(),
+			question: fake.randParagraph(),
+			type: 'FREE_TEXT',
+			expected_answer: null,
+			options: [],
+			correct_options: [],
+			boolean_expected_answer: null,
+			image_url: null,
+
+			exam_id: examId,
+
+			updated_at: new Date(),
+			created_at: new Date(),
+		}
 	}
+
 }
